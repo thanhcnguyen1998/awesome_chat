ea282cf0edf09f715b32207db358156c4c2a1121
thanhcnguyen1998 <smokerdev98@gmail.com>
2020-03-12 16:29:23 +0700

    [1.0.0] Init Helloworld

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+var hostname = "localhost";
+var port = 1998;
+
+app.get("/helloworld", (req, res)=>{
+    res.send("<h1>Hello cac ban</h1>");
+});
+
+app.listen(port, hostname, ()=>{
+    console.log('Running....');
+});
